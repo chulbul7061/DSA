f7985b2225c6f67852f593d331918ba8bb6dbc11
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-10 00:26:27 +0530

    Update singlylinked_list_1.c

--- singlylinked_list_1.c
+++ singlylinked_list_1.c
@@ -1,43 +1,45 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-  struct node
-  {
-    int data;
-    struct node *next;
-
-  }
-
-
-struct node *head , *newnode , *temp;
-head=NULL;
-int choice;
-
-while(choice)
-{
-newnode=(struct node*)malloc(sizeof(struct node));
-printf("enter the data");
-
-scanf("%d",&newnode->data);
-newnode->next=NULL;
-
-  if(head==NULL)
+    struct node
     {
-         head=temp=newnode;
-    }
-    else
-    {
-      temp->next=newnode;
-      temp=newnode;
-    }
-    printf("do you want to continue enter 1 or 0 to exit()");
-    scanf("%d",&choice);
-}
-  temp=head;
-  while(temp!=0)
-  {
-    printf("%d",&temp->data);
-    temp=temp->next;
+        int data;
+        struct node *next;
     
-  }
+        
+    };
+
+    struct node *head , *newnode, *temp;
+    head =0;int choice;
+    while (choice)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("enter the data\n");
+        scanf("%d",&newnode->data);
+        newnode->next=0;
+
+        if(head==0)
+        {
+            head=temp=newnode;
+        }
+        else
+        {
+            temp->next=newnode;
+            temp=newnode;
+        }
+
+
+        printf("do you want to continue (0,1)?\n");
+      scanf("%d",&choice);  
+    }
+    temp=head;
+    while (temp!=0)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+    
+    
+    
 }
