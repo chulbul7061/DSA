ec0f200102cb1922167837a17c5418a1f88612a1
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-09 23:36:52 +0530

    singlylinked_list_1.c

--- singlylinked_list_1.c
+++ singlylinked_list_1.c
@@ -1,5 +1,43 @@
 #include<stdio.h>
 int main()
 {
-  printf("hellow");
+  struct node
+  {
+    int data;
+    struct node *next;
+
+  }
+
+
+struct node *head , *newnode , *temp;
+head=NULL;
+int choice;
+
+while(choice)
+{
+newnode=(struct node*)malloc(sizeof(struct node));
+printf("enter the data");
+
+scanf("%d",&newnode->data);
+newnode->next=NULL;
+
+  if(head==NULL)
+    {
+         head=temp=newnode;
+    }
+    else
+    {
+      temp->next=newnode;
+      temp=newnode;
+    }
+    printf("do you want to continue enter 1 or 0 to exit()");
+    scanf("%d",&choice);
+}
+  temp=head;
+  while(temp!=0)
+  {
+    printf("%d",&temp->data);
+    temp=temp->next;
+    
+  }
 }
