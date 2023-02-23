887a3006e26db2ecd01b39ceb41cb46afd31a12d
HIMANSHU SHEKHAR <98458674+chulbul7061@users.noreply.github.com>
2023-02-23 13:44:03 +0530

    Create_delete_Printing

--- singlylinked_list_1.c
+++ singlylinked_list_1.c
@@ -1,45 +1,151 @@
 #include<stdio.h>
 #include<stdlib.h>
-int main()
-{
-    struct node
+#include<malloc.h>
+ struct node
     {
         int data;
         struct node *next;
-    
         
     };
 
-    struct node *head , *newnode, *temp;
-    head =0;int choice;
-    while (choice)
+    struct node *newnode,*temp,*head;
+    //head=NULL;
+
+void insert(int numberOfNode)
+{
+     for(int i=1;i<=numberOfNode;i++)
+  {
+    newnode=(struct node*)malloc(sizeof(struct node));
+
+    printf("enter the data of newnode ");
+    scanf("%d",&newnode->data);
+
+    newnode->next=NULL;
+
+    if(head==NULL)
     {
-        newnode=(struct node*)malloc(sizeof(struct node));
-        printf("enter the data\n");
-        scanf("%d",&newnode->data);
-        newnode->next=0;
-
-        if(head==0)
-        {
-            head=temp=newnode;
-        }
-        else
-        {
-            temp->next=newnode;
-            temp=newnode;
-        }
-
-
-        printf("do you want to continue (0,1)?\n");
-      scanf("%d",&choice);  
+        head=temp=newnode;
     }
+
+    else{
+
+        temp->next=newnode;
+        temp=newnode;
+         }
+  }
+
+};
+
+
+    void printing()
+    {
+        temp=head;
+        while (temp!=NULL)
+        {
+            printf("%d\n",temp->data);
+            temp=temp->next;
+         }
+    } ;
+
+void insertNewFirst()
+{
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("enter the data of newnode at first position ");
+    scanf("%d",&newnode->data);
+
+    newnode->next=head;
+    head=newnode;
+    
+};
+
+void insertLastPosition()
+{
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("\nenter the data of newnode at last position  ");
+    scanf("%d",&newnode->data);
+
+    newnode->next=NULL;
     temp=head;
-    while (temp!=0)
+    while (temp->next!=0)                   
     {
-        printf("%d\n",temp->data);
+       temp=temp->next;  
+    }
+    temp->next=newnode;
+    
+};
+
+void deleteBeg()
+{
+    printf("+++++++++++++++++ TRYING TO DELETE AT FIRST POSITION  +++++++++++++++++++\n");
+    if(head==NULL)
+    {
+        printf("Your Linked is empty currently \n");
+    }
+    else
+    {
+        temp=head;
+        head=temp->next;
+        free(temp);
+    }
+
+};
+
+void deleteLast()
+{
+    printf("@@@@@@@@@@@@@@@@@@@@@@@ DELETE LAST POSITION @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n");
+    struct node *Previous;
+    temp=head;
+    while (temp->next!=0)
+    {
+        Previous=temp;
         temp=temp->next;
+
     }
     
+    if(temp==head)
+    {
+        head=NULL;
+        free(temp);
+    }
+
+    else{
+    Previous->next=NULL;
+    }
+    free(temp);
+
     
+}
+
+int main()
+{
+   
+    int numberOfNode;
+    printf("How many node you want to create\n");
+    scanf("%d",&numberOfNode);
+  
+ 
+     insert(numberOfNode) ;
+     printing();
+     insertNewFirst();
+     printf("----------------------------------\n");
+     printing();
+     printf("----------------------------------\n");
+     insertLastPosition();
+     printf("----------------------------------\n");
+     printing();
+     printf("----------------------------------\n");
+
+    deleteBeg();
+    printf("====================================\n");
+    printing();
+    printf("====================================\n");
+
+    deleteLast();
+    printf("====================================\n");
+    printing();
+    printf("====================================\n");
+
+ return 0;
+
     
 }
